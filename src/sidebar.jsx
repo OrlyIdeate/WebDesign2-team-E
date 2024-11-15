3cdac4aaa6356a3048ef83b67d05c654bae5a70f
OrlyIdeate <ueno.jin25@outlook.jp>
2024-11-15 18:44:43 +0900

    refactor: サイドバーのメニュー構造を簡素化し、状態管理を削除

--- src/sidebar.jsx
+++ src/sidebar.jsx
@@ -2,68 +2,49 @@
 import React, { useState } from 'react';
 
 export function Sidebar() {
-  
-    const Sidebar = () => {
-      // メニュー項目の状態管理
-      const [activeMenu, setActiveMenu] = useState(null);
-    
-      // メニュー項目をクリックしたときの動作
-      const toggleMenu = (menu) => {
-        setActiveMenu((prevActiveMenu) => (prevActiveMenu === menu ? null : menu));
-      };
-    
-      return (
-        <div className="sidebar">
-          <ul className="menu">
-            <li>
-              <a href="#" className="menu-item" onClick={() => toggleMenu('categories')}>
-                商品カテゴリ
-              </a>
-              {activeMenu === 'categories' && (
-                <ul className="submenu">
-                  <li><a href="#">ファッション</a></li>
-                  <li><a href="#">家電</a></li>
-                  <li><a href="#">食品</a></li>
-                  <li><a href="#">スポーツ・アウトドア</a></li>
-                  <li><a href="#">美容・健康</a></li>
-                  <li><a href="#">キッズ・ベビー用品</a></li>
-                  <li><a href="#">家具・インテリア</a></li>
-                  <li><a href="#">ペット用品</a></li>
-                  <li><a href="#">車・バイク用品</a></li>
-                  <li><a href="#">本・メディア</a></li>
-                  <li><a href="#">ガジェット・アクセサリー</a></li>
-                  <li><a href="#">ギフト・季節用品</a></li>
-                  </ul>
-              )}
-            </li>
-            <li>
-              <a href="#" className="menu-item" onClick={() => toggleMenu('services')}>
-                サービス
-              </a>
-              {activeMenu === 'services' && (
-                <ul className="submenu">
-                  <li><a href="#">配送サービス</a></li>
-                  <li><a href="#">ギフトカード</a></li>
-                </ul>
-              )}
-            </li>
-            <li>
-              <a href="#" className="menu-item" onClick={() => toggleMenu('account')}>
-                マイアカウント
-              </a>
-              {activeMenu === 'account' && (
-                <ul className="submenu">
-                  <li><a href="#">ログイン</a></li>
-                  <li><a href="#">新規登録</a></li>
-                  <li><a href="#">注文履歴</a></li>
-                </ul>
-              )}
-            </li>
+  return (
+    <div className="sidebar">
+      <h2>メニュー</h2>
+      <ul className="menu">
+        <li>
+          <a href="#" className="menu-item">
+            商品カテゴリ
+          </a>
+          <ul className="submenu">
+            <li><a href="#">ファッション</a></li>
+            <li><a href="#">家電</a></li>
+            <li><a href="#">食品</a></li>
+            <li><a href="#">スポーツ・アウトドア</a></li>
+            <li><a href="#">美容・健康</a></li>
+            <li><a href="#">キッズ・ベビー用品</a></li>
+            <li><a href="#">家具・インテリア</a></li>
+            <li><a href="#">ペット用品</a></li>
+            <li><a href="#">車・バイク用品</a></li>
+            <li><a href="#">本・メディア</a></li>
+            <li><a href="#">ガジェット・アクセサリー</a></li>
+            <li><a href="#">ギフト・季節用品</a></li>
+            </ul>
+        </li>
+        <li>
+          <a href="#" className="menu-item">
+            サービス
+          </a>
+          <ul className="submenu">
+            <li><a href="#">配送サービス</a></li>
+            <li><a href="#">ギフトカード</a></li>
           </ul>
-        </div>
-      );
-    };
-    
-    
-    
+        </li>
+        <li>
+          <a href="#" className="menu-item">
+            マイアカウント
+          </a>
+          <ul className="submenu">
+            <li><a href="#">ログイン</a></li>
+            <li><a href="#">新規登録</a></li>
+            <li><a href="#">注文履歴</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  );
 }
