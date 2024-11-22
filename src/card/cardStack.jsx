60169ee17e97a3b3d472ee5e3a150bf68e3ab79c
OrlyIdeate <ueno.jin25@outlook.jp>
2024-11-22 18:07:07 +0900

    カードコンポーネントのスタイルを微調整し、カードスタックのレイアウトを改善

--- src/card/cardStack.jsx
+++ src/card/cardStack.jsx
@@ -4,13 +4,13 @@ import React from 'react';
 import { Card } from './card';
 export function CardStack(props) {
     return (
-        <div style={{ width: '400px', height: '400px', marginLeft: '10', backgroundColor: '#FFFFFF' }}>
+        <div style={{ width: '350px', height: '350px', margin: '0 auto', backgroundColor: '#FFFFFF' }}>
             <h3 style={{ textAlign: 'center', marginBottom: '0'}}>{props.title}</h3>
-            <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+            <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center', alignItems: 'center' }}>
                 <Card imageSrc={props.image1} />
                 <Card imageSrc={props.image2} />
             </div>
-            <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+            <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center', alignItems: 'center' }}>
                 <Card imageSrc={props.image3} />
                 <Card imageSrc={props.image4} />
             </div>
